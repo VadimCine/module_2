6120bf8cb904a636ebd2a6229b65d5c8159982c4
VadimCine <aka-sem@yandex.ru>
2024-08-31 11:39:36 +0300

    Create module_2_4.py

---
+++ module_2_4.py
@@ -0,0 +1,13 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in range(2, 16):
+    for n in range(2, int(i ** 0.5) + 2):
+        if i % n != 0:
+            primes.append(i)
+            break
+        else:
+            not_primes.append(i)
+            break
+print(primes)
+print(not_primes)
