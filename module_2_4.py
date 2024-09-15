34a857a4b509c639ede3aafbbcc370fbef2132e8
VadimCine <aka-sem@yandex.ru>
2024-09-15 15:29:48 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,13 +1,20 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for i in range(2, 16): # или здесь можно поставить  for i in range(len(numbers) + 1)? Но тогда в расчет попадает 1(
-    for n in range(2, int(i ** 0.5) + 2):
-        if i % n != 0:
-            primes.append(i)
+for i in numbers:
+    is_prime = True
+    if i < 2:
+        is_prime = False
+        if i == 1:
+            continue
+    for divisor in range(2, int(i**0.5) + 1):
+        if i % divisor == 0:
+            is_prime = False
             break
-        else:
-            not_primes.append(i)
-            break
-print(primes)
-print(not_primes)
+    if is_prime:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+
+print("Простые числа:", primes)
+print("Непростые числа:", not_primes)
